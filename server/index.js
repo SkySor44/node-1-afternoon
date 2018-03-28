addeac7789e05716c1c6b25c4594b25cfbf7b74c
Skyler Sorensen <skylersorensen@Skylers-MacBook-Air.local>
2018-03-28 15:15:52 -0600

    finished

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mc = require('./controllers/messages_controller')
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../public/build'))
+
+
+app.post('/api/messages', mc.create)
+app.get('/api/messages', mc.read)
+app.put('/api/messages/:id', mc.update)
+app.delete('/api/messages/:id', mc.delete)
+
+
+
+
+
+
+app.listen(3000, () => {console.log("I am listening at port 3000");
+})
